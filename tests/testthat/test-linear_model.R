16888003d9ca28b1c98175d6594c01f588772a12
Mauro Lepore <maurolepore@gmail.com>
2024-10-08 19:14:30 +0000

    Test

--- tests/testthat/test-linear_model.R
+++ tests/testthat/test-linear_model.R
@@ -10,3 +10,10 @@ test_that("yields an 'lm'", {
 
   expect_s3_class(out, "lm")
 })
+
+test_that("is minimalistic and only takes `formula`", {
+  x <- 1:3
+  y <- 2 * x
+  expect_error(linear_model(y ~ x, data = mtcars), "unused")
+})
+
