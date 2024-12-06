e1e29fef6a4a9e5bf2d9f456f4a4534eed3982cd
Mauro Lepore <mauro@ixpantia.com>
2024-12-06 20:39:54 +0000

    Documentar.

--- R/greet.R
+++ R/greet.R
@@ -1,3 +1,13 @@
+#' Di hola
+#'
+#' @param name String.
+#'
+#' @return String.
+#' @export
+#'
+#' @examples
+#' greet()
+#' greet('Mauro')
 greet <- function(name = NULL) {
   name <- name %||% "world"
   paste("hello", name)
